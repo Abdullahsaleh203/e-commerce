0eb3ed9a18ef3b638d2434a8c5d5e7916fb40d56
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-05-24 14:54:25 +0300

    refactor: comment out cart and product route handlers for future implementation

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', async (req, res) => {
-    const products = await Product.find();
-    res.json(products);
-  });
+// router.get('/', async (req, res) => {
+//     const products = await Product.find();
+//     res.json(products);
+//   });
   
-  router.post('/', authMiddleware, adminMiddleware, async (req, res) => {
-    const product = new Product(req.body);
-    await product.save();
-    res.status(201).json(product);
-  });
+//   router.post('/', authMiddleware, adminMiddleware, async (req, res) => {
+//     const product = new Product(req.body);
+//     await product.save();
+//     res.status(201).json(product);
+//   });
