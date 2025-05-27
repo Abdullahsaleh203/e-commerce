d1d35582c22686e63e3311abd88d771f8776f98e
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-05-27 21:01:56 +0300

    feat: update getFeaturedProducts to handle missing cache and improve error handling

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -10,8 +10,20 @@ export const getAllProducts = asyncHandler(async (req, res, next) => {
 
 export const getFeaturedProducts =  asyncHandler(async (req, res, next) => { 
     let FeaturedProducts = await redis.get("featured_products");
-    if (!FeaturedProducts){
-        return res.status(404).json({massage:"No featured products found"})
+    // If found in cache, return the cached data
+    if (FeaturedProducts) {
+        res.json(JSON.parse(FeaturedProducts));
     }
+    // if not in redis, fetch from mongodb
+    // .lean() is used to return plain JavaScript objects instead of Mongoose documents
+    // which is more efficient for read operations for performance
+    FeaturedProducts = await Product.find({ featured: true }).lean().sort({ createdAt: -1 });
     
+    // If not found in cache, fetch from database
+    if (!FeaturedProducts) {
+        return next(new AppError("No featured products found", 404));
+    }
+    // Store in Redis cache for future access
+    await redis.set("featured_products", JSON.stringify(FeaturedProducts));
+    res.json(FeaturedProducts );
 })
--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { protect , restrictTo} from '../middleware/authMiddleware.js';
-import { getAllProducts } from '../controllers/productController.js';
+import { getAllProducts, getFeaturedProducts } from '../controllers/productController.js';
 
 
 router.get('/products', protect, restrictTo('admin'), getAllProducts);
-
+router.get('/featured-products', getFeaturedProducts);
 
 export default router;
