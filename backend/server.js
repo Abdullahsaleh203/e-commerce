5de4f73213ed492e4b179613f406fd887f681a9b
Abdallah Saleh <132389571+Abdullahsaleh203@users.noreply.github.com>
2025-05-29 22:06:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import  userRouter  from './routes/userRouter.js';
 import productsRouter from './routes/productsRouter.js';
 
 const app = express();
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
